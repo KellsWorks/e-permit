8669e3793b199b3e74594595341ed2fd3558927b
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-10-10 18:52:35 +0200

    Refactor Home + Header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import Wildlife from '../assets/wildlife-malawi-bg-2.jpg'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
 
@@ -7,20 +6,20 @@ const Home = () => {
         <div>
             {/* Hero section */}
             <section className="text-gray-600 body-font">
-                <div className="container bg-cover w-full h-128 flex px-10 py-5 md:flex-row flex-col items-center" style={{ backgroundImage: `url(${Wildlife})` }}>
+                <div className="bg-cover w-full flex pr-5 pl-5 pt-5 pb-10 sm:pr-20 sm:pt-20 sm:pb-40 sm:pl-20 md:flex-row flex-col items-center" style={{ backgroundImage: `url(https://lp-cms-production.imgix.net/features/2016/09/GettyImages-578179271_full-e3d250fd7575.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850)` }}>
                     <div className="lg:flex-grow w-full flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-semibold text-white">Department of National Parks and Wildlife - Malawi
                     </h1>
-                    <p className="mb-8 leading-relaxed text-gray-200">Welcome to the DNPW Electronic Permit Application Portal (ePermit).</p>
+                    <p className="mb-8 leading-relaxed text-gray-100">Welcome to the DNPW Electronic Permit Application Portal (ePermit).</p>
                     <div className="flex justify-center">
-                        <button className="inline-flex text-white transition duration-150 bg-green-500 border-0 py-2 px-4 focus:outline-none hover:bg-green-600 rounded-sm text-md">Get started</button>
+                        <Link to="/permit-application" className="inline-flex text-white transition duration-150 bg-green-500 border-0 py-2 px-4 focus:outline-none hover:bg-green-600 rounded-sm text-md">Get started</Link>
                     </div>
                     </div>
                 </div>
                 </section>
 
                 {/* Get started */}
-                <div className="container px-5 sm:py-24 py-5 mx-auto flex flex-wrap border-gray-100 mt-2" id="getStarted">
+                <div id="getStarted" className="container px-5 sm:py-24 py-5 mb-5 mx-auto flex flex-wrap border-gray-100 mt-2">
                     <section className="text-gray-600 body-font">
                         <div className="container sm:px-24 px-5 sm:py-5 mx-auto">
                             <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
@@ -33,11 +32,11 @@ const Home = () => {
                                     <div className="flex-grow pl-6">
                                     <h2 className="text-gray-900 text-lg title-font font-semibold mb-2">Register</h2>
                                     <p className="leading-relaxed text-base">Obtaining a wildlife permit is now easier with this online portal. Create an account and get started.</p>
-                                    <a href="#" className="mt-3 text-blue-500 hover:text-blue-700 transition duration-150 font-semibold inline-flex items-center">Learn More
+                                    <Link to="/sign-up" className="mt-3 text-blue-500 hover:text-blue-700 transition duration-150 font-semibold inline-flex items-center">Learn More
                                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                                         </svg>
-                                    </a>
+                                    </Link>
                                 </div>
                             </div>
                             <div className="p-4 md:w-1/3 flex">
@@ -49,28 +48,27 @@ const Home = () => {
                                 <div className="flex-grow pl-6">
                                 <h2 className="text-gray-900 text-lg title-font font-semibold mb-2">Permit &amp; Licence Guide</h2>
                                 <p className="leading-relaxed text-base">Learn more about the available permits, licenses and their associated fees and application requirements.</p>
-                                <a href="{{ route('permits-guide') }}" className="mt-3 text-blue-500 hover:text-blue-700 transition duration-150 font-semibold inline-flex items-center">Learn More
+                                <Link to="/licenses-and-permits" className="mt-3 text-blue-500 hover:text-blue-700 transition duration-150 font-semibold inline-flex items-center">Learn More
                                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                                     </svg>
-                                </a>
+                                </Link>
                                 </div>
                             </div>
                             <div className="p-4 md:w-1/3 flex">
                                 <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-green-500 shadow hover:bg-green-700 text-white mb-4 flex-shrink-0">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" />
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0" />
-                                </svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
                                 </div>
                                 <div className="flex-grow pl-6">
                                 <h2 className="text-gray-900 text-lg title-font font-semibold mb-2">International Regulation</h2>
                                 <p className="leading-relaxed text-base">Need to export or import from outside? Learn about the CITE procedures for international regulation.</p>
-                                <a href="{{ route('application-guide') }}" className="mt-3 text-blue-500 hover:text-blue-700 transition duration-150 font-semibold inline-flex items-center">Learn More
+                                <Link to="/international-regulations" className="mt-3 text-blue-500 hover:text-blue-700 transition duration-150 font-semibold inline-flex items-center">Learn More
                                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                                     </svg>
-                                </a>
+                                </Link>
                                 </div>
                             </div>
                             </div>
--- src/pages/layouts/LandingPageHeader.tsx
+++ src/pages/layouts/LandingPageHeader.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-
 import { Fragment, useEffect, useState } from 'react'
 import { Popover, Transition, Menu } from '@headlessui/react'
 import {
@@ -20,7 +18,7 @@ import { Link, useHistory } from 'react-router-dom'
 import CookieService from '../../services/CookieService'
 import UrlService from '../../services/UrlService'
 
-const solutions = [
+const menuHeaderLists = [
   {
     name: 'Application Guide',
     description: 'Get a better understanding of where your traffic is coming from.',
@@ -47,7 +45,8 @@ const solutions = [
     icon: QuestionMarkCircleIcon,
   },
 ]
-const resources = [
+
+const menuFooterLists = [
   {
     name: 'About us',
     description: 'Get all of your questions answered in our forums or contact support.',
@@ -68,7 +67,6 @@ const resources = [
   },
   { name: 'Usage Policy', description: 'Understand how we take your privacy seriously.', href: '#', icon: ShieldCheckIcon },
 ]
-
 
 export default function LandingPageHeader() {
 
@@ -100,191 +98,210 @@ export default function LandingPageHeader() {
   }, [])
 
   return (
-      <Popover className="relative bg-green-500">
-    <div className="max-w-7xl mx-auto px-4 sm:px-6">
-      <div className="flex justify-between items-center py-6 md:justify-start md:space-x-10">
-        <div className="flex justify-start lg:w-0 lg:flex-1">
-          <Link to="/" className="flex items-center">
-            <img
-              className="h-8 w-auto sm:h-10"
-              src={Icon}
-              alt="favicon"
-            />
-            <h3 className="sm:hidden font-semibold ml-3 text-white hover:text-gray-300 transition duration-150">DNPW E-Permitting</h3>
+    <Popover className="relative bg-green-500">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="flex justify-between items-center py-6 md:justify-start md:space-x-10">
+          <div className="flex justify-start lg:w-0 lg:flex-1">
+            <Link to="/" className="flex items-center">
+              <img
+                className="h-8 w-auto sm:h-10"
+                src={Icon}
+                alt="favicon"
+              />
+              <h3 className="sm:hidden font-semibold ml-3 text-white hover:text-gray-300 transition duration-150">DNPW E-Permitting</h3>
+              
+            </Link>
+          </div>
+          <div className="-mr-2 -my-2 md:hidden">
+            <Popover.Button className="bg-green-600 rounded-sm p-2 inline-flex items-center justify-center text-white hover:text-gray-300 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
+              <span className="sr-only">Open menu</span>
+              <MenuIcon className="h-6 w-6" aria-hidden="true" />
+            </Popover.Button>
+          </div>
+          <Popover.Group as="nav" className="hidden md:flex space-x-10">
             
-          </Link>
-        </div>
-        <div className="-mr-2 -my-2 md:hidden">
-          <Popover.Button className="bg-green-600 rounded-sm p-2 inline-flex items-center justify-center text-white hover:text-gray-300 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
-            <span className="sr-only">Open menu</span>
-            <MenuIcon className="h-6 w-6" aria-hidden="true" />
-          </Popover.Button>
-        </div>
-        <Popover.Group as="nav" className="hidden md:flex space-x-10">
-          
-          <Link to="/application-guide" className="text-base font-medium text-white hover:text-gray-200">
-              Application Guide
-          </Link>
+            <Link to="/application-guide" className="text-base font-medium text-white hover:text-gray-200">
+                Application Guide
+            </Link>
 
-          <Link to="/licenses-and-permits" className="text-base font-medium text-white hover:text-gray-200">
-              Licenses & Permits
-          </Link>
+            <Link to="/licenses-and-permits" className="text-base font-medium text-white hover:text-gray-200">
+                Licenses & Permits
+            </Link>
 
-          <Link to="/international-regulations" className="text-base font-medium text-white hover:text-gray-200">
-              International Regulation
-          </Link>
+            <Link to="/international-regulations" className="text-base font-medium text-white hover:text-gray-200">
+                International Regulation
+            </Link>
 
-          <Link to="/wildlife" className="text-base font-medium text-white hover:text-gray-200">
-              Wildlife Act
-          </Link>
+            <Link to="/wildlife" className="text-base font-medium text-white hover:text-gray-200">
+                Wildlife Act
+            </Link>
 
-          <Link to="/faq" className="text-base font-medium text-white hover:text-gray-200">
-              FAQ
-          </Link>
+            <Link to="/faq" className="text-base font-medium text-white hover:text-gray-200">
+                FAQ
+            </Link>
 
-        </Popover.Group>
-        {
-          CookieService.get("access_token") ? <div className="hidden md:flex items-center space-x-4">
-            
-            {/* <button className="bg-gray-600 p-1 rounded-full text-gray-300 hover:text-white focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-offset-gray-800 focus:ring-white">
-              <span className="sr-only">View notifications</span>
-
-              <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-              </svg>
-
-            </button> */}
-            <Menu as="div" className="ml-3 relative">
-              <div>
-                <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-offset-gray-800 focus:ring-white">
-                  <span className="sr-only">Open user menu</span>
-                  <img className="h-8 w-8 rounded-full focus:ring-0 focus:outline-none" src={"http://localhost:8000/storage/images/profile/" + user } alt="avatar" />
-                </Menu.Button>
+          </Popover.Group>
+          {
+            CookieService.get("access_token") ? <div className="hidden md:flex items-center space-x-4">
+              
+              <Menu as="div" className="ml-3 relative">
+                <div>
+                  <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-offset-gray-800 focus:ring-white">
+                    <span className="sr-only">Open user menu</span>
+                    <img className="h-8 w-8 rounded-full focus:ring-0 focus:outline-none" src={"http://localhost:8000/storage/images/profile/" + user } alt="avatar" />
+                  </Menu.Button>
+                </div>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-sm shadow-lg py-1 bg-white border ring-0 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                          <Link to="/permit-application"
+                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 transition duration-150"
+                          >
+                            Start application
+                          </Link>
+                      </Menu.Item>
+                      <Menu.Item>
+                          <Link to="/profile"
+                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 transition duration-150"
+                          >
+                            Profile
+                          </Link>
+                      </Menu.Item><Menu.Item>
+                          <Link to="#"
+                            onClick={() => {signOut()}}
+                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-red-500 hover:text-white transition duration-150"
+                          >
+                            Log out
+                          </Link>
+                      </Menu.Item>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
               </div>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
-              >
-                <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-sm shadow-lg py-1 bg-white border ring-0 ring-black ring-opacity-5 focus:outline-none">
-                    <Menu.Item>
-                        <Link to="/permit-application"
-                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 transition duration-150"
-                        >
-                          Start application
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                        <Link to="/profile"
-                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 transition duration-150"
-                        >
-                          Profile
-                        </Link>
-                    </Menu.Item><Menu.Item>
-                        <Link to="#"
-                          onClick={() => {signOut()}}
-                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-red-500 hover:text-white transition duration-150"
-                        >
-                          Log out
-                        </Link>
-                    </Menu.Item>
-                </Menu.Items>
-              </Transition>
-            </Menu>
-            </div>
-          : <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-          <Link to="/sign-in" className="whitespace-nowrap text-base font-medium text-white hover:text-gray-200">
-            Sign in
-          </Link>
-          <Link
-            to="/sign-up"
-            className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-sm shadow-sm text-base font-medium text-white bg-green-600 hover:bg-green-700"
-          >
-            Sign up
-          </Link>
+            : <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+            <Link to="/sign-in" className="whitespace-nowrap text-base font-medium text-white hover:text-gray-200">
+              Sign in
+            </Link>
+            <Link
+              to="/sign-up"
+              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-sm shadow-sm text-base font-medium text-white bg-green-600 hover:bg-green-700"
+            >
+              Sign up
+            </Link>
+          </div>
+          }
         </div>
-        }
       </div>
-    </div>
 
-    <Transition
-      as={Fragment}
-      enter="duration-200 ease-out"
-      enterFrom="opacity-0 scale-95"
-      enterTo="opacity-100 scale-100"
-      leave="duration-100 ease-in"
-      leaveFrom="opacity-100 scale-100"
-      leaveTo="opacity-0 scale-95"
-    >
-      <Popover.Panel focus className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-        <div className="rounded-sm border shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-          <div className="pt-5 pb-6 px-5">
-            <div className="flex items-center justify-between">
-              <Link to="/" className="flex items-center">
-                <img
-                  className="h-8 w-auto sm:h-10"
-                  src={Icon}
-                  alt="favicon"
-                />
-                <h3 className="sm:hidden font-semibold ml-3 text-gray-600 hover:text-gray-300 transition duration-150">DNPW E-Permitting</h3>
-                
-              </Link>
-              <div className="-mr-2">
-                <Popover.Button className="bg-white rounded-sm p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
-                  <span className="sr-only">Close menu</span>
-                  <XIcon className="h-6 w-6" aria-hidden="true" />
-                </Popover.Button>
+      <Transition
+        as={Fragment}
+        enter="duration-200 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <Popover.Panel focus className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+          <div className="rounded-sm border shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+            <div className="pt-5 pb-6 px-5">
+              <div className="flex items-center justify-between">
+                <Link to="/" className="flex items-center">
+                  <img
+                    className="h-8 w-auto sm:h-10"
+                    src={Icon}
+                    alt="favicon"
+                  />
+                  <h3 className="sm:hidden font-semibold ml-3 text-gray-600 hover:text-gray-300 transition duration-150">DNPW E-Permitting</h3>
+                  
+                </Link>
+                <div className="-mr-2">
+                  <Popover.Button className="bg-white rounded-sm p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
+                    <span className="sr-only">Close menu</span>
+                    <XIcon className="h-6 w-6" aria-hidden="true" />
+                  </Popover.Button>
+                </div>
+              </div>
+              <div className="mt-6">
+                <nav className="grid gap-y-8">
+                  {menuHeaderLists.map((item) => (
+                    <Link
+                      key={item.name}
+                      to={item.href}
+                      className="-m-3 p-3 flex items-center rounded-sm hover:bg-gray-50"
+                    >
+                      <item.icon className="flex-shrink-0 h-6 w-6 text-green-600" aria-hidden="true" />
+                      <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
+                    </Link>
+                  ))}
+                </nav>
               </div>
             </div>
-            <div className="mt-6">
-              <nav className="grid gap-y-8">
-                {solutions.map((item) => (
+            <div className="py-6 px-5 space-y-6">
+              <div className="grid grid-cols-2 gap-y-4 gap-x-8">
+                {menuFooterLists.map((item) => (
                   <Link
                     key={item.name}
                     to={item.href}
-                    className="-m-3 p-3 flex items-center rounded-sm hover:bg-gray-50"
+                    className="text-base font-medium text-gray-900 hover:text-gray-700"
                   >
-                    <item.icon className="flex-shrink-0 h-6 w-6 text-green-600" aria-hidden="true" />
-                    <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
+                    {item.name}
                   </Link>
                 ))}
-              </nav>
-            </div>
-          </div>
-          <div className="py-6 px-5 space-y-6">
-            <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-              {resources.map((item) => (
+              </div>
+              {
+                CookieService.get("access_token") ?
+                <div>
+                <div className="grid grid-cols-2 gap-y-4 gap-x-8">
                 <Link
-                  key={item.name}
-                  to={item.href}
+                  to="/permit-application"
                   className="text-base font-medium text-gray-900 hover:text-gray-700"
                 >
-                  {item.name}
+                  Start application
                 </Link>
-              ))}
-            </div>
-            <div>
+                <Link
+                  to="/profile"
+                  className="text-base font-medium text-gray-900 hover:text-gray-700"
+                >
+                  Profile
+                </Link>
+                
+              </div>
               <Link
-                to="/sign-up"
-                className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-sm shadow-sm text-base font-medium text-white bg-green-600 hover:bg-green-700"
-              >
-                Sign up
-              </Link>
-              <p className="mt-6 text-center text-base font-medium text-gray-500">
-                Existing member?{' '}
-                <Link to="/sign-in" className="text-green-600 hover:text-green-500">
-                  Sign in
+                  to="#"
+                  onClick={() => signOut()}
+                  className="w-full mt-3 flex items-center justify-center px-4 py-2 border border-transparent rounded-sm shadow-sm text-base font-medium text-white bg-red-600 hover:bg-red-700"
+                >
+                  Log out
                 </Link>
-              </p>
+              </div> : <div>
+                <Link
+                  to="/sign-up"
+                  className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-sm shadow-sm text-base font-medium text-white bg-green-600 hover:bg-green-700"
+                >
+                  Sign up
+                </Link>
+                <p className="mt-6 text-center text-base font-medium text-gray-500">
+                  Existing member?{' '}
+                  <Link to="/sign-in" className="text-green-600 hover:text-green-500">
+                    Sign in
+                  </Link>
+                </p>
+              </div>
+              }
+              
             </div>
           </div>
-        </div>
-      </Popover.Panel>
-    </Transition>
-  </Popover>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
   )
 }
